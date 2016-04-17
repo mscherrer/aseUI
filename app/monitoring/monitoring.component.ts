2e60a418080d3001e2ee5386ad9e6f74b013af08
Matthias <matthias@ubuntu.ubuntu-domain>
2016-04-17 17:26:22 +0200

    charts

--- app/monitoring/monitoring.component.ts
+++ app/monitoring/monitoring.component.ts
@@ -1,8 +1,30 @@
 import {Component} from 'angular2/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from 'angular2/common';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 
 
 @Component({
   selector: 'monitoring',
-  templateUrl: 'app/monitoring/monitoring.component.html'
+  templateUrl: 'app/monitoring/monitoring.component.html',
+  directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
-export class MonitoringAnalysis {}
+export class MonitoringAnalysis {
+constructor() {
+    console.log('pie demo');
+  }
+
+  // events
+  chartClicked(e:any) {
+    console.log(e);
+  }
+
+  chartHovered(e:any) {
+    console.log(e);
+  }
+
+  // Pie
+  private pieChartLabels = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  private pieChartData = [300, 500, 100];
+  private pieChartType = 'Pie';
+}
+
