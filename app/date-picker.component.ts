cf9573e606602b0c5d791718d5ad88705fd56b6b
Matthias <matthias@ubuntu.ubuntu-domain>
2016-05-26 17:03:33 +0200

    datepicker from to update

--- app/date-picker.component.ts
+++ app/date-picker.component.ts
@@ -4,7 +4,6 @@ import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import * as moment from 'moment';
 import {DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
-
 @Component({
   selector: 'date-picker-demo',
   templateUrl: 'app/date-picker.component.html',
@@ -12,8 +11,8 @@ import {DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 })
 
 export class DatePickerComponent {
-  public dt:Date = new Date();
-  public yesterday:Date = new Date();
+  public pickerToDate:Date = new Date();
+  public pickerFromDate:Date = new Date();
   public events:Array<any>;
   public formats:Array<string> = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY', 'shortDate'];
   public format:string = this.formats[0];
@@ -24,20 +23,7 @@ export class DatePickerComponent {
   private opened:boolean = false;
 
   public constructor() {
-    this.yesterday.setDate(this.dt.getDate() - 1);
-  }
-
-  public getDate():number {
-    return this.dt && this.dt.getTime() || new Date().getTime();
-  }
-
-  public getYesterdayDate():Date {
-    this.dt.setDate(this.dt.getDate() - 1);
-    return this.dt;
-  }
-
-  public today():void {
-    this.dt = new Date();
+    this.pickerFromDate.setDate(this.pickerFromDate.getDate() - 1);
   }
 
   // todo: implement custom class cases
@@ -63,9 +49,5 @@ export class DatePickerComponent {
 
   public open():void {
     this.opened = !this.opened;
-  }
-
-  public clear():void {
-    this.dt = void 0;
   }
 }
